9b54d66b746173551b9e0e6d95c4614cc9deb233
Aibek Ayazbek <aayazbek1936@gmail.com>
2024-03-18 13:01:56 +0500

    removed unnecessary imports

--- test/controllers/users.controller.spec.ts
+++ test/controllers/users.controller.spec.ts
@@ -1,19 +1,13 @@
 import { INestApplication } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { SequelizeConfigService } from 'src/config/sequelizeConfig.service';
-import { Test, TestingModule } from '@nestjs/testing';
-import { databaseConfig } from 'src/config/configuration';
-import { UsersModule } from 'src/users/users.module';
-import { User } from 'src/users/users.model';
 import * as bcrypt from 'bcrypt';
 import * as request from 'supertest';
-
-const mockedUser = {
-  username: 'Jhon',
-  email: 'jhon@gmail.com',
-  password: 'jhon123',
-};
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Test, TestingModule } from '@nestjs/testing';
+import { databaseConfig } from 'src/config/configuration';
+import { SequelizeConfigService } from 'src/config/sequelizeConfig.service';
+import { User } from 'src/users/users.model';
+import { UsersModule } from 'src/users/users.module';
 
 describe('Users Controller', () => {
   let app: INestApplication;
@@ -36,20 +30,7 @@ describe('Users Controller', () => {
     await app.init();
   });
 
-  beforeEach(async () => {
-    const user = new User();
-
-    const hashedPassword = await bcrypt.hash(mockedUser.password, 10);
-
-    user.username = mockedUser.username;
-    user.password = hashedPassword;
-    user.email = mockedUser.email;
-
-    return user.save();
-  });
-
   afterEach(async () => {
-    await User.destroy({ where: { username: mockedUser.username } });
     await User.destroy({ where: { username: 'Test' } });
   });
 
