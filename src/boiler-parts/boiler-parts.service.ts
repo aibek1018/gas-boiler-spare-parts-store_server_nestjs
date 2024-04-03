4121c6ecf5a2c816c9721a632cf777cc679523db
Aibek Ayazbek <aayazbek1936@gmail.com>
2024-04-03 09:26:02 +0500

    changed paginateAndFilter

--- src/boiler-parts/boiler-parts.service.ts
+++ src/boiler-parts/boiler-parts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { BoilerParts } from './boiler-parts.model';
 import { InjectModel } from '@nestjs/sequelize';
-import { IBoilerPartsQuery } from './types';
+import { IBoilerPartsFilter, IBoilerPartsQuery } from './types';
 import { Op } from 'sequelize';
 
 @Injectable()
@@ -16,9 +16,25 @@ export class BoilerPartsService {
   ): Promise<{ count: number; rows: BoilerParts[] }> {
     const limit = +query.limit;
     const offset = +query.offset * 20;
+    const filter = {} as Partial<IBoilerPartsFilter>;
+
+    if (query.priceFrom && query.priceTo) {
+      filter.price = {
+        [Op.between]: [+query.priceFrom, +query.priceTo],
+      };
+    }
+
+    if (query.boiler) {
+      filter.boiler_manufacturer = JSON.parse(decodeURIComponent(query.boiler));
+    }
+
+    if (query.parts) {
+      filter.parts_manufacturer = JSON.parse(decodeURIComponent(query.parts));
+    }
     return this.boilerPartsModel.findAndCountAll({
       limit,
       offset,
+      where: filter,
     });
   }
 
